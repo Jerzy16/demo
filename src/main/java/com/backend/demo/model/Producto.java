e97961d56995c0270da2070ae3ffe932817de648
jerzy <113630990+Jerzy16@users.noreply.github.com>
2026-05-13 17:37:51 -0500

    correciones

--- src/main/java/com/backend/demo/model/Producto.java
+++ src/main/java/com/backend/demo/model/Producto.java
@@ -19,11 +19,28 @@ public class Producto {
         this.precio = precio;
     }
 
-    public String getId() { return id; }
+    public String getId() {
+        return id;
+    }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    // <-- AGREGA ESTO
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public double getPrecio() { return precio; }
-    public void setPrecio(double precio) { this.precio = precio; }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
