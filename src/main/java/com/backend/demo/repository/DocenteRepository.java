eacbc63ab87e932fb32eca45a4abd98ba0d2d506
jerzy <113630990+Jerzy16@users.noreply.github.com>
2026-05-22 12:50:53 -0500

    Revert "subida de back"
    This reverts commit 422dd0e9b91d56bf7eafb8288a4458ac4afc1337.

--- src/main/java/com/backend/demo/repository/DocenteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.demo.repository;
-
-import com.backend.demo.model.Docente;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DocenteRepository extends MongoRepository<Docente, String> {
-
-}
